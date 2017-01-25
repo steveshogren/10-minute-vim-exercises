ab3d2e136faff6551144995ebdc8d0889e5838af
jack <steve.a.shogren@gmail.com>
2017-01-25 07:21:49 -0500

    New files for macro register changes

---
+++ users-update.sql
@@ -0,0 +1,10 @@
+-- insert into db01.users (username, password) values ('jack', 'changeme');
+-- update db01.users set password='changeme' where username='jack';
+--password,username,db
+changeme,jack,db01
+password,janet,db02
+Passw0rd,john,db01
+thewordpasswordallcaps,jane,db03
+secrete,jimothy,db03
+password2,june,db01
+password1,jose,db02
---
+++ users.sql
@@ -0,0 +1,9 @@
+-- insert into db01.users (username, password) values ('jack', 'changeme');
+--password,username,db
+changeme,jack,db01
+password,janet,db02
+Passw0rd,john,db01
+thewordpasswordallcaps,jane,db03
+secrete,jimothy,db03
+password2,june,db01
+password1,jose,db02
